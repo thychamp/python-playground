806a118e0ff5530b8d3aa9ef3adc9ca5621df2f1
smdebauch-hash <smdebauch@gmail.com>
2025-09-18 04:06:16 +0000

    getting started

---
+++ thursday.py
@@ -0,0 +1,16 @@
+
+#forloop
+siblings = ["skye", "kyra", "dane"]
+
+for name in siblings:
+    print(name)
+
+print(siblings)
+
+#boolean
+isTired = False
+
+if isTired: 
+    print("skye is tired")
+else:
+    print("skye is ready for the day")
